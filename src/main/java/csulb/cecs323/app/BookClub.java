c7e65477bf764c1ab621e357fdcd96b7edb3948f
Tony Samaniego <tms.ocmp@gmail.com>
2021-10-23 14:24:19 -0700

    Initial Commit

---
+++ src/main/java/csulb/cecs323/app/BookClub.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed under the Academic Free License (AFL 3.0).
+ *     http://opensource.org/licenses/AFL-3.0
+ *
+ *  This code is distributed to CSULB students in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, other than educational.
+ *
+ *  2018 Alvaro Monge <alvaro.monge@csulb.edu>
+ *
+ */
+
+package csulb.cecs323.app;
+
+// Import all of the entity classes that we have written for this application.
+import csulb.cecs323.model.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple application to demonstrate how to persist an object in JPA.
+ * <p>
+ * This is for demonstration and educational purposes only.
+ * </p>
+ * <p>
+ *     Originally provided by Dr. Alvaro Monge of CSULB, and subsequently modified by Dave Brown.
+ * </p>
+ */
+public class BookClub {
+   /**
+    * You will likely need the entityManager in a great many functions throughout your application.
+    * Rather than make this a global variable, we will make it an instance variable within the BookClub
+    * class, and create an instance of BookClub in the main.
+    */
+   private EntityManager entityManager;
+
+   /**
+    * The Logger can easily be configured to log to a file, rather than, or in addition to, the console.
+    * We use it because it is easy to control how much or how little logging gets done without having to
+    * go through the application and comment out/uncomment code and run the risk of introducing a bug.
+    * Here also, we want to make sure that the one Logger instance is readily available throughout the
+    * application, without resorting to creating a global variable.
+    */
+   private static final Logger LOGGER = Logger.getLogger(BookClub.class.getName());
+
+   /**
+    * The constructor for the BookClub class.  All that it does is stash the provided EntityManager
+    * for use later in the application.
+    * @param manager    The EntityManager that we will use.
+    */
+   public BookClub(EntityManager manager) {
+      this.entityManager = manager;
+   }
+
+   public static void main(String[] args) {
+      LOGGER.fine("Creating EntityManagerFactory and EntityManager");
+      LOGGER.setLevel(Level.OFF);
+      EntityManagerFactory factory = Persistence.createEntityManagerFactory("BookClub");
+      EntityManager manager = factory.createEntityManager();
+      // Create an instance of BookClub and store our new EntityManager as an instance variable.
+      BookClub bookclub = new BookClub(manager);
+
+
+      // Any changes to the database need to be done within a transaction.
+      // See: https://en.wikibooks.org/wiki/Java_Persistence/Transactions
+
+   } // End of the main method
+
+   /**
+    * Create and persist a list of objects to the database.
+    * @param entities   The list of entities to persist.  These can be any object that has been
+    *                   properly annotated in JPA and marked as "persistable."  I specifically
+    *                   used a Java generic so that I did not have to write this over and over.
+    */
+   public <E> void createEntity(List <E> entities) {
+      for (E next : entities) {
+         LOGGER.info("Persisting: " + next);
+         // Use the BookClub entityManager instance variable to get our EntityManager.
+         this.entityManager.persist(next);
+      }
+
+      // The auto generated ID (if present) is not passed in to the constructor since JPA will
+      // generate a value.  So the previous for loop will not show a value for the ID.  But
+      // now that the Entity has been persisted, JPA has generated the ID and filled that in.
+      for (E next : entities) {
+         LOGGER.info("Persisted object after flush (non-null id): " + next);
+      }
+   } // End of createEntity member method
+} // End of BookClub class
