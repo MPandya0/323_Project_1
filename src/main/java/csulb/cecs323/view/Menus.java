9ee3553ba26fa449f10b5c329203d03d92a9e4a8
Tony Samaniego <tms.ocmp@gmail.com>
2021-10-31 11:28:48 -0700

    Created Menus class

---
+++ src/main/java/csulb/cecs323/view/Menus.java
@@ -0,0 +1,14 @@
+package csulb.cecs323.view;
+
+public class Menus {
+
+    public static void mainMenu(){
+        System.out.println("MAIN MENU");
+        System.out.println(" 1) Add New Object");
+        System.out.println(" 2) List All Object information");
+        System.out.println(" 3) Delete Book");
+        System.out.println(" 4) Update a Book");
+        System.out.println(" 5) List All Primary Keys");
+        System.out.println(" 6) Exit Program");
+    }
+}
