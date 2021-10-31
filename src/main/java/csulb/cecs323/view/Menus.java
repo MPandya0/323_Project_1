4d4533cd8a0c1ac462df2cba58837477fec04891
Tony Samaniego <tms.ocmp@gmail.com>
2021-10-31 14:29:58 -0700

    Modified Format

--- src/main/java/csulb/cecs323/view/Menus.java
+++ src/main/java/csulb/cecs323/view/Menus.java
@@ -3,12 +3,20 @@ package csulb.cecs323.view;
 public class Menus {
 
     public static void mainMenu(){
-        System.out.println("MAIN MENU");
-        System.out.println(" 1) Add New Object");
-        System.out.println(" 2) List All Object information");
-        System.out.println(" 3) Delete Book");
-        System.out.println(" 4) Update a Book");
-        System.out.println(" 5) List All Primary Keys");
-        System.out.println(" 6) Exit Program");
+        System.out.println("\nMAIN MENU");
+        System.out.println("  1) Add New Object");
+        System.out.println("  2) List All Object information");
+        System.out.println("  3) Delete Book");
+        System.out.println("  4) Update a Book");
+        System.out.println("  5) List All Primary Keys");
+        System.out.println("  6) Exit Program");
+    }
+
+    public static void createNewItemMainMenu() {
+        System.out.println("\nWhat kind of object would you like to insert?");
+        System.out.println("  1) Authoring Entity");
+        System.out.println("  2) Publisher");
+        System.out.println("  3) Book");
+        System.out.println("  4) Go back");
     }
 }
